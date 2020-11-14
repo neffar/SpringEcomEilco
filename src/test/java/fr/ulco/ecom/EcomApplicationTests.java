f4448e8fb37a5a9146fd66268b63e2aabbdf10bf
neffar <soufiane.neffar@gmail.com>
2020-11-14 17:03:16 +0100

    Initial commit 2

---
+++ src/test/java/fr/ulco/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ulco.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
