8a9f330fca524d3544a9051315422fc5978e43c7
neffar <soufiane.neffar@gmail.com>
2020-11-19 23:51:17 +0100

    Change template engine to thymleaf

--- src/main/java/fr/ulco/ecom/controller/IndexController.java
+++ src/main/java/fr/ulco/ecom/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/index")
 public class IndexController {
 
     @RequestMapping(value = "/")
