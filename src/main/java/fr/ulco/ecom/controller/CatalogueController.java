26edbadf5c1bee769b4628ea90193765efd531f8
neffar <soufiane.neffar@gmail.com>
2020-11-19 18:14:16 +0100

    Added basic routes

--- src/main/java/fr/ulco/ecom/controller/CatalogueController.java
+++ src/main/java/fr/ulco/ecom/controller/CatalogueController.java
@@ -1,11 +1,13 @@
 package fr.ulco.ecom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * contient les services d’accès au catalogue
  * de la e-boutique, en particulier pour l’accès aux catégoriesaux et aux produits
  */
 @Controller
+@RequestMapping(value = "/catalogue")
 public class CatalogueController {
 }
--- src/main/java/fr/ulco/ecom/controller/CommandeController.java
+++ src/main/java/fr/ulco/ecom/controller/CommandeController.java
@@ -1,11 +1,12 @@
 package fr.ulco.ecom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * contient les services de gestion des commandes
- *
  */
 @Controller
+@RequestMapping(value = "/commande")
 public class CommandeController {
 }
