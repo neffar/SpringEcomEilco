758eb29b67dafc6a9bff5b27881170d0011f76b6
neffar <soufiane.neffar@gmail.com>
2020-11-21 00:01:48 +0100

    Added registration process

--- src/main/java/fr/ulco/ecom/controller/ClientController.java
+++ src/main/java/fr/ulco/ecom/controller/ClientController.java
@@ -1,10 +1,16 @@
 package fr.ulco.ecom.controller;
 
 import fr.ulco.ecom.dao.ClientRepository;
+import fr.ulco.ecom.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping(value = "/client")
@@ -17,13 +23,18 @@ public class ClientController {
         this.clientRepository = clientRepository;
     }
 
-    @RequestMapping(value = "")
-    public String register(Model model) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String register() {
         return "auth/register";
     }
 
-    @RequestMapping(value = "/register")
-    public String addClient(Model model) {
-        return null;
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public void addClient(@ModelAttribute("client") Client client, BindingResult result, HttpServletResponse response) throws IOException {
+        if (!result.hasErrors()) {
+            client.setAuthority("ROLE_USER");
+            client.setEnabled((short) 1);
+            clientRepository.save(client);
+        }
+        response.sendRedirect("/login");
     }
 }
