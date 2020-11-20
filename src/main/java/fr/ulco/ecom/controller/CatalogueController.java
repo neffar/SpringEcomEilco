62eb8840d32c14cf9edcf7298d426fa6928cd18d
neffar <soufiane.neffar@gmail.com>
2020-11-20 03:58:48 +0100

    Made Ajax REST controller system

--- src/main/java/fr/ulco/ecom/controller/CatalogueController.java
+++ src/main/java/fr/ulco/ecom/controller/CatalogueController.java
@@ -1,13 +1,40 @@
 package fr.ulco.ecom.controller;
 
-import org.springframework.stereotype.Controller;
+import fr.ulco.ecom.dao.CategorieRepository;
+import fr.ulco.ecom.dao.ProduitRepository;
+import fr.ulco.ecom.model.Categorie;
+import fr.ulco.ecom.model.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * contient les services d’accès au catalogue
  * de la e-boutique, en particulier pour l’accès aux catégoriesaux et aux produits
  */
-@Controller
-@RequestMapping(value = "/catalogue")
+@RestController
+@RequestMapping(value = "/api")
 public class CatalogueController {
+
+    private CategorieRepository categorieRepository;
+    private ProduitRepository produitRepository;
+
+    @Autowired
+    public CatalogueController(CategorieRepository categorieRepository, ProduitRepository produitRepository) {
+        this.categorieRepository = categorieRepository;
+        this.produitRepository = produitRepository;
+    }
+
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
+    public List<Categorie> getCategories() {
+        return categorieRepository.findAll();
+    }
+
+    @RequestMapping(value = "/produits", method = RequestMethod.GET)
+    public List<Produit> getProduits() {
+        return produitRepository.findAll();
+    }
 }
