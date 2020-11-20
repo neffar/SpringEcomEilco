b928200dfba58ee0b12fcef1f9d29419dbde3221
neffar <soufiane.neffar@gmail.com>
2020-11-20 23:32:55 +0100

    Added regitration page and controller route

--- src/main/java/fr/ulco/ecom/controller/ClientController.java
+++ src/main/java/fr/ulco/ecom/controller/ClientController.java
@@ -1,9 +1,29 @@
 package fr.ulco.ecom.controller;
 
+import fr.ulco.ecom.dao.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/client")
 public class ClientController {
+
+    final private ClientRepository clientRepository;
+
+    @Autowired
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @RequestMapping(value = "")
+    public String register(Model model) {
+        return "auth/register";
+    }
+
+    @RequestMapping(value = "/register")
+    public String addClient(Model model) {
+        return null;
+    }
 }
