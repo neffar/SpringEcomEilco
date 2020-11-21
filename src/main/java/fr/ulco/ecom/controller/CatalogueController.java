311ab1c2299166bdccb8bdaf2036567a6f8f4451
neffar <soufiane.neffar@gmail.com>
2020-11-21 15:40:01 +0100

    Secured Catalogue route

--- src/main/java/fr/ulco/ecom/controller/CatalogueController.java
+++ src/main/java/fr/ulco/ecom/controller/CatalogueController.java
@@ -2,12 +2,22 @@ package fr.ulco.ecom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * contient les services d’accès au catalogue
  * de la e-boutique, en particulier pour l’accès aux catégories et aux produits
  */
 @Controller
-@RequestMapping(value = "/user/catalogue")
 public class CatalogueController {
+
+    @RequestMapping(value = "/user/catalogue", method = RequestMethod.GET)
+    public String redirect1() {
+        return "redirect:/user";
+    }
+
+    @RequestMapping(value = "/catalogue", method = RequestMethod.GET)
+    public String redirect2() {
+        return "redirect:/user";
+    }
 }
